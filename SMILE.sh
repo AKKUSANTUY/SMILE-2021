5ba123f91cb080ee086bd0a69927cfbe2063baa4
⎝◣⏝◢⎠SANTRI-CYBER-SANTUY⎝◣⏝◢⎠ <79558956+AKKUSANTUY@users.noreply.github.com>
2021-04-05 11:14:38 +0700

    Create SMILE.sh

---
+++ SMILE.sh
@@ -0,0 +1,411 @@
+GNU nano 5.6.1                                                                      terserah.sh
+#!/bin/sh
+#JANGAN RECODE AJG LU MAMPU NGA BOS BISA NYA RECODE BNGSD LU
+
+
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+
+clear
+echo
+toilet -f big -F gay SANTRI CYBER
+echo $green"SCRIPT SMILE 2021 05.04.2021"
+echo $blue"######################################"
+echo $purple"AUTHOR    : SANTRI CYBER SANTUY"
+echo $purple"INSTAGRAM : MR VIRUS SPM"
+echo $purple"SCRIPT    : SMILE 2021 05.04.2021"
+echo $purple"TINGKAT   : NEWBIE TO MASTAH"
+echo $purple"WHATSAPP  : +62 821-3456-6596
+echo $blue"######################################"
+sleep 2
+echo
+echo $blue"================================="
+echo $green"| SILAHKAN PILIH TOOLS MANA CUK |"
+echo $blue"================================="
+echo
+sleep 1
+echo $yellow"1.) spam sms ke 1"
+echo "•••••••••••••••••••••••••••"
+sleep 1
+echo $yellow"2.) spam sms ke 2"
+echo "•••••••••••••••••••••••••••"
+sleep 1
+echo $yellow"3.) spam sms ke 3"
+echo "•••••••••••••••••••••••••••"
+sleep 1
+echo $yellow"4.) spam call"
+echo "•••••••••••••••••••••••••••"
+sleep 1
+echo $yellow"5.) hack cctv 1"
+echo "•••••••••••••••••••••••••••"
+sleep 1
+echo $yellow"6.) hack cctv 2"
+echo "•••••••••••••••••••••••••••"
+sleep 1
+echo $yellow"7.) hack cctv 3"
+echo "•••••••••••••••••••••••••••"
+sleep 1
+echo $yellow"8.) spam call 1"
+echo "•••••••••••••••••••••••••••"
+sleep 1
+echo $yellow"9.) spam call 2"
+echo "•••••••••••••••••••••••••••"
+sleep 1
+echo $yellow"10.) spam call 3"
+echo "•••••••••••••••••••••••••••"
+sleep 1
+echo $yellow"11.) ddos ke 1"
+echo "•••••••••••••••••••••••••••"
+sleep 1
+echo $yellow"12.) ddos ke 2"
+echo "•••••••••••••••••••••••••••"
+sleep 1
+echo $yellow"13.) ddos ke 3"
+echo "•••••••••••••••••••••••••••"
+sleep 1
+echo $yellow"14.) virus link"
+echo "•••••••••••••••••••••••••••"
+sleep 1
+echo $yellow"15.) virus vbug"
+echo "•••••••••••••••••••••••••••"
+sleep 1
+echo $yellow"16.) tool bajingan"
+echo "•••••••••••••••••••••••••••"
+sleep 1
+echo $yellow"17.) tool unfaedah"
+echo "•••••••••••••••••••••••••••"
+sleep 1
+echo $yellow"18.) tool tuan badut"
+echo "•••••••••••••••••••••••••••"
+sleep 1
+echo $red"19.)Mau Keluar Cuk"
+echo
+sleep 1
+read -p "[SCS] MASUKAN NOMER TOOLS CUK : " gan
+
+if [ $gan = 1 ]
+then
+echo $green"NAH SCRIPTNYA SEMOGA BERMANFAAT"
+echo $blue"==============================="
+echo
+sleep 1
+echo $red"#########################################"
+echo $yellow"pkg update && pkg upgrade"
+echo $yellow"pkg install python"
+echo $yellow"pkg install git"
+echo $yellow"git clone https://github.com/KANG-NEWBIE/dokOTP"
+echo $yellow"cd dokOTP"
+echo $yellow"pip install bs4 requests"
+echo $yellow"python halodoc.py"
+echo $red"##########################################"
+fi
+
+if [ $gan = 2 ]
+then
+echo $green"NIH SCRIPTNYA SEMOGA BERMANFAAT"
+echo $blue"==============================="
+sleep 1
+echo
+echo $red"#########################################"
+echo $yellow"pkg update && pkg upgrade"
+echo $yellow"pkg install git"
+echo $yellow"pkg install python"
+echo $yellow"pkg install requests"
+echo $yellow"git clone https://github.com/xractz/fsms"
+echo $yellow"cd fsms"
+echo $yellow"python fsms.py"
+echo $red"#########################################"
+fi
+
+if [ $gan = 3 ]
+then
+echo $green"NIH SCRIPTNYA SEMOGA BERMANFAAT"
+echo $blue"================================"
+sleep 1
+echo
+echo $red"#########################################"
+echo $yellow"pkg install python2"
+echo $yellow"pkg install git"
+echo $yellow"pip2 install requests"
+echo $yellow"git clone https://github.com/rezadkim/spammerZ"
+echo $yellow"cd spammerZ"
+echo $yellow"python2 spam.py"
+echo $red"#########################################"
+fi
+
+if [ $gan = 4 ]
+then
+echo $green"NIH SCRIPTNYA SEMOGA BERMANFAAT"
+echo $blue"==============================="
+sleep 1
+echo
+echo $red"#########################################"
+echo $yellow"pkg update && pkg upgrade"
+echo $yellow"pkg install python"
+echo $yellow"pkg install python2"
+echo $yellow"git clone https://github.com/MRV1RU55PM/MDSpammers"
+echo $yellow"cd MDSpammers"
+echo $yellow"pip2 install requests"
+echo $yellow"python2 mvs.py"
+echo $red"#########################################"
+fi
+
+if [ $gan = 5 ]
+then
+echo $green"NIH SCRIPTNYA SEMOGA BERMANFAAT"
+echo $blue"==============================="
+sleep 1
+echo
+echo $red"#########################################"
+echo $yellow"pkg update && pkg upgrade "
+echo $yellow"pkg install python2 -y"
+echo $yellow"pkg install git -y"
+echo $yellow"git clone https://github.com/Kancotdiq/ipcs"
+echo $yellow"cd ipcs"
+echo $yellow"pip2 install requests"
+echo $yellow"python2 scan.py"
+echo $red"#########################################"
+fi
+
+if [ $gan = 6 ]
+then
+echo $green"NIH SCRIPTNYA SEMOGA BERMANFAAT"
+echo $blue"==============================="
+sleep 1
+echo
+echo $red"#########################################"
+echo $yellow"pkg update && pkg upgrade -y"
+echo $yellow"pkg install python2"
+echo $yellow"pkg install git"
+echo $yellow"git clone https://github.com/GUNAWAN18ID/cctv.git"
+echo $yellow"pip2 install requests"
+echo $yellow"cd cctv"
+echo $yellow"python2 scanner.py"
+echo $red"#########################################"
+fi
+
+if [ $gan = 7 ]
+then
+echo $green"NIH SCRIPTNYA SEMOGA BERMANFAAT"
+echo $blue"==============================="
+sleep 1
+echo
+echo $red"#########################################"
+echo $yellow"apt-get install python3"
+echo $yellow"apt-get install git"
+echo $yellow"git clone https://github.com/AngelSecurityTeam/Cam-Hackers"
+echo $yellow"pip3 install requests"
+echo $yellow"pip3 install colorama"
+echo $yellow"cd Cam-Hackers"
+echo $yellow"python3 cam-hackers.py"
+echo $red"#########################################"
+fi
+
+if [ $gan = 8 ]
+then
+echo $green"NIH SCRIPTNYA SEMOGA BERMANFAAT"
+echo $blue"================================"
+sleep 1
+echo
+echo $red"#########################################"
+echo $yellow"pkg update && pkg upgrade"
+echo $yellow"pkg install git"
+echo $yellow"pkg install bash"
+echo $yellow"pkg install bash4"
+echo $yellow"pkg install figlet"
+echo $yellow"pkg install lolcat"
+echo $yellow"git clone https://github.com/TrioChanel/trio2"
+echo $yellow"cd trio2"
+echo $yellow"bash trio1"
+echo $red"#########################################"
+fi
+
+if [ $gan = 9 ]
+then
+echo $green"NIH SCRIPTNYA SEMOGA BERMANFAAT"
+echo $blue"==============================="
+sleep 1
+echo
+echo $red"#########################################"
+echo $yellow"pkg install python"
+echo $yellow"pkg install python2"
+echo $yellow"pkg install git"
+echo $yellow"git clone https://github.com/ridwanszz15/spamcallsms"
+echo $yellow"cd spamcallsms"
+echo $yellow"pip2 install requests"
+echo $yellow"python2 main.py"
+echo $red"#########################################"
+fi
+
+if [ $gan = 10 ]
+then
+echo $green"NIH SCRIPTNYA SEMOGA BERMANFAAT"
+echo $blue"==============================="
+sleep 1
+echo
+echo $red"#########################################"
+echo $yellow"pkg install git -y"
+echo $yellow"pkg install python2"
+echo $yellow"pip2 install requests"
+echo $yellow"git clone https://github.com/Mr-S4NTUY/spam-call"
+echo $yellow"cd spam-call"
+echo $yellow"python2 main.py"
+echo $red"#########################################"
+fi
+
+if [ $gan = 11 ]
+then
+echo $green"NIH SCRIPTNYA SEMOGA BERMANFAAT"
+echo $blue"==============================="
+sleep 1
+echo
+echo $red"#########################################"
+echo $green"WIN 3 DDOS ATTACK"
+echo
+echo $yellow"pkg update && pkg upgrade"
+echo $yellow"pkg install git toilet"
+echo $yellow"pkg install python"
+echo $yellow"pkg install python2"
+echo $yellow"git clone https://github.com/banghyuu/ddosWD"
+echo $yellow"cd ddosWD"
+echo $yellow"sh Ddos.sh"
+echo $red"#########################################"
+fi
+
+if [ $gan = 12 ]
+then
+echo $green"NIH SCRIPTNYA SEMOGA BERMANFAAT"
+echo $blue"==============================="
+sleep 1
+echo
+echo $blue"==============================="
+echo $yellow"pkg install git"
+echo $yellow"git clone https://github.com/BlackCyberAnonim/B-ATTACKING"
+echo $yellow"cd B-ATTACKING"
+echo $yellow"sh install.sh"
+echo $blue"==============================="
+fi
+
+if [ $gan = 13 ]
+then
+echo $green"NIH SCRIPTNYA SEMOGA BERMANFAAT"
+echo $blue"==============================="
+echo
+sleep 1
+echo $red"#########################################"
+echo $yellow"pkg update && pkg upgrade"
+echo $yellow"pkg install figlet"
+echo $yellow"pkg install python"
+echo $yellow"git clone https://github.com/MRV1RU55PM/DDos-Attack"
+echo $yellow"cd DDos-Attack"
+echo $yellow"python2 DDOSMRVIRUS.py"
+echo $red"#########################################"
+fi
+
+if [ $gan = 14 ]
+then
+echo $green"NIH SCRIPTNYA SEMOGA BERMANFAAT"
+echo $blue"==============================="
+echo
+sleep 1
+echo $red"#########################################"
+echo $yellow"pkg update && pkg upgrade"
+echo $yellow"pkg install git"                                                                                                                                                    echo $yellow"pkg install lolcat"
+echo $yellow"git clone https://github.com/noob-hackers/infect"
+echo $yellow"cd infect"
+echo $yellow"bash infect.sh"
+echo $red"#########################################"
+fi
+
+if [ $gan = 15 ]
+then
+echo $green"NIH SCRIPTNYA SEMOGA BERMANFAAT"
+echo $blue"==============================="
+echo
+sleep 1
+echo $red"#########################################"
+echo $yellow"pkg update && pkg upgrade"
+echo $yellow"pkg install git"
+echo $yellow"pkg install python"
+echo $yellow"pkg install python2"
+echo $yellow"termux-setup-storage"
+echo $yellow"git clone https://github.com/Gameye98/vbug"
+echo $yellow"cd vbug"
+echo $yellow"python2 vbug.py"
+echo $yellow"pilih nama virus"
+echo $yellow"mv namavirus.apk cd/sdcard/"
+echo $red"#########################################"
+fi
+
+if [ $gan = 16 ]
+then
+echo $green"NIH SCRIPTNYA SEMOGA BERMANFAAT"
+echo $blue"==============================="
+echo
+sleep 1
+echo $red"#########################################"
+echo $yellow"pkg install git"
+echo $yellow"git clone https://github.com/DarknessCyberTeam/BAJINGANv6"
+echo $yellow"cd BAJINGANv6"
+echo $yellow"sh BAJINGAN.sh"
+echo $yellow"USERNAME:BAJINGAN"
+echo $yellow"PASSWORD:Gans"
+echo $red"#########################################"
+fi
+
+if [ $gan = 17 ]
+then
+echo $green"NIH SCRIPTNYA SEMOGA BERMANFAAT"
+echo $blue"==============================="
+echo
+sleep 1
+echo $red"#########################################"
+echo $yellow"apt update && apt upgrade"
+echo $yellow"pkg install python git"
+echo $yellow"git clone https://github.com/Spx-ID/UNFAEDAH"
+echo $yellow"cd UNFAEDAH"
+echo $yellow"pip install -r requirements.txt"
+echo $yellow"python main.py"
+echo $red"#########################################"
+fi
+
+if [ $gan = 18 ]
+then
+echo $green"NIH SCRIPTNYA SEMOGA BERMANFAAT"
+echo $blue"==============================="
+echo
+sleep 1                                                                                                               echo $red"#########################################"
+echo $yellow"apt update && apt upgrade"                                                                               echo $yellow"apt install git"
+echo $yellow"apt install curl"
+echo $yellow"apt install ruby"
+echo $yellow"apt install figlet"
+echo $yellow"apt install python2"                                                                                     echo $yellow"apt install lolcat"
+echo $yellow"git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv3"
+echo $yellow"cd TOOLSINSTALLERv3"
+echo $yellow"chmod +x TUANB4DUT.sh"                                                                                   echo $yellow"sh TUANB4DUT.sh"
+echo $red"#########################################"
+fi
+
+if [ $gan = 19 ]
+then
+echo $blue"======================================="
+echo $green"TRIMAKASIH SUDAH MENGUNAKAN TOOLS INI"
+echo $yellow"JANGAN LUPA FOLLOW IG KU @AKKU_SANTUY"
+echo $blue"======================================="
+echo
+sleep 2
+echo $yellow"YAUDAH TINGGAL EXIT AJA KONTOL"
+echo $green"JANGAN LUPA ISTIGHFAR KITA SEMUA PENDOSA"
+echo $blue"######################################"
+echo $purple"AUTHOR    : SANTRI CYBER SANTUY"
+echo $purple"INSTAGRAM : MR VIRUS SPM"
+echo $purple"SCRIPT    : SMILE 2021 05.04.2021"
+echo $purple"TINGKAT   : NEWBIE TO MASTAH"
+echo $purple"WHATSAPP  : +62 821-3456-6596
+echo $blue"######################################"
+fi
